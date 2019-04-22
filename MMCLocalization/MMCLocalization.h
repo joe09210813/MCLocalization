934cdfe643f79a190b861f51164ec57a489c9da8
李政修 <joe_lee@mitake.com.tw>
2019-04-22 15:27:30 +0800

    Add MMCLocaolization Framework

---
+++ MMCLocalization/MMCLocalization.h
@@ -0,0 +1,23 @@
+//
+//  MMCLocalization.h
+//  MMCLocalization
+//
+//  Created by 李政修 on 2019/4/22.
+//  Copyright © 2019 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMCLocalization.
+FOUNDATION_EXPORT double MMCLocalizationVersionNumber;
+
+//! Project version string for MMCLocalization.
+FOUNDATION_EXPORT const unsigned char MMCLocalizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMCLocalization/PublicHeader.h>
+
+#import <MMCLocalization/MCLocalization.h>
+#import <MMCLocalization/MCLocalizationDataSource.h>
+#import <MMCLocalization/MCLocalizationDummyDataSource.h>
+#import <MMCLocalization/MCLocalizationSingleJSONFileDataSource.h>
+#import <MMCLocalization/MCLocalizationOneJSONFilePerLanguageDataSource.h>
